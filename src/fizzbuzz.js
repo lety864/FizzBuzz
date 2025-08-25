1f05f8aedfaf241234ae9ee9e3016f06c3b80603
Leticia Janet BR <bermudez.leticia.2609@gmail.com>
2025-08-25 12:03:54 -0600

    Se agrego el codigo al archivo fizzbuzz.js

--- src/fizzbuzz.js
+++ src/fizzbuzz.js
@@ -1,4 +1,21 @@
+
 export function fizzbuzz(){
-  //tu código va aqui
-  
+    let i=1;
+    let n = 100;
+
+    while(i<=n){
+        if(i%3==0 && i%5==0) {
+            console.log("FizzBuzz")
+        }else if(i%3==0){
+            console.log("Fizz");
+        }else if(i%5==0){
+            console.log("Buzz");
+        }else{
+            console.log(i);
+        }
+        i++;
+    }
 }
+
+let n = fizzbuzz();
+console.log(n);
